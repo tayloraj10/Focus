a0cfc834fb18cdde2495aea0a8a7ea2725ca918c
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-03-28 01:40:48 -0400

    add delete button to FocusCard and implement NewFocusDialog in FocusList

--- src/components/focus-card.tsx
+++ src/components/focus-card.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Focus } from '../slices/focusSlice';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 interface FocusCardProps {
     focus: Focus
@@ -7,9 +10,18 @@ interface FocusCardProps {
 
 const FocusCard: React.FC<FocusCardProps> = ({ focus }) => {
     return (
-        <div className="focus-card border border-gray-300 rounded-lg p-6 shadow-md">
-            <h2 className="focus-card-title text-xl font-semibold mb-2">{focus.name}</h2>
-            <p className="focus-card-description text-gray-700">{focus.category}</p>
+        <div className="focus-card mb-10 border border-gray-300 rounded-lg p-6 shadow-md">
+            <div className="flex justify-between items-center">
+                <div>
+                    <h2 className="focus-card-title text-xl font-semibold mb-2">{focus.name}</h2>
+                    <p className="focus-card-description text-gray-700">{focus.category}</p>
+                </div>
+                <div>
+                    <IconButton aria-label="add" style={{ marginTop: '0.5rem' }} onClick={() => { }} >
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/focus-list.tsx
+++ src/components/focus-list.tsx
@@ -3,6 +3,7 @@ import { Focus } from '../slices/focusSlice';
 import FocusCard from './focus-card';
 import { IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import NewFocusDialog from './new-focus-dialog';
 
 
 
@@ -11,15 +12,18 @@ interface FocusListProps {
 }
 
 const FocusList: React.FC<FocusListProps> = ({ items }) => {
+    const [showAddDialog, setShowAddDialog] = React.useState(false);
+
     return (
         <div>
             <div className="flex flex-row items-center justify-center mb-4">
                 <h1 className='text-3xl font-bold flex items-center'>
                     Your Focuses
                 </h1>
-                <IconButton aria-label="add" style={{ marginTop: '0.5rem' }}  >
+                <IconButton aria-label="add" style={{ marginTop: '0.5rem' }} onClick={() => setShowAddDialog(true)} >
                     <AddIcon style={{ fontSize: '2rem' }} />
                 </IconButton>
+                <NewFocusDialog open={showAddDialog} onClose={() => setShowAddDialog(false)} />
             </div>
             <ul>
                 {items.map((item) => (
