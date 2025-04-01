6da8ce9221971aaca84204a965d2bb774dc71bdd
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-04-01 17:59:50 -0400

    add ability to delete focuses

--- src/components/focus-card.tsx
+++ src/components/focus-card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Focus } from '../slices/focusSlice';
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { supabaseConnection } from '../supabase/supabaseClient';
 
 
 interface FocusCardProps {
@@ -9,6 +10,25 @@ interface FocusCardProps {
 }
 
 const FocusCard: React.FC<FocusCardProps> = ({ focus }) => {
+    const deleteFocus = async (focusID: string) => {
+        try {
+            console.log(`Deleting focus with ID: ${focusID}`);
+            const { error } = await supabaseConnection
+                .from('focuses')
+                .delete()
+                .eq('id', focusID);
+
+            if (error) {
+                throw new Error(`Error deleting focus: ${error.message}`);
+            } else {
+                console.log('Focus deleted successfully');
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
     return (
         <div className="focus-card mb-10 border border-gray-300 rounded-lg p-6 shadow-md">
             <div className="flex justify-between items-center">
@@ -17,7 +37,7 @@ const FocusCard: React.FC<FocusCardProps> = ({ focus }) => {
                     <p className="focus-card-description text-gray-700">{focus.category}</p>
                 </div>
                 <div>
-                    <IconButton aria-label="add" style={{ marginTop: '0.5rem' }} onClick={() => { }} >
+                    <IconButton aria-label="add" style={{ marginTop: '0.5rem' }} onClick={() => { deleteFocus(focus.id) }} >
                         <DeleteIcon />
                     </IconButton>
                 </div>
