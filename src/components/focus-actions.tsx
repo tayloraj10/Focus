53cfda373182c7bf520bc400ddd85f91c5ba1387
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-04-01 20:05:12 -0400

    feat: update addAction to set action date based on the most recent action

--- src/components/focus-actions.tsx
+++ src/components/focus-actions.tsx
@@ -31,16 +31,26 @@ const FocusActions: React.FC<FocusActionsProps> = ({ focusID, actions }) => {
     };
 
     const addAction = async () => {
+        let newDate = new Date();
+        if (actions.length > 0) {
+            const mostRecentAction = actions.reduce((latest, action) => {
+                const actionDate = new Date(action.date);
+                return actionDate > new Date(latest.date) ? action : latest;
+            }, actions[0]);
+            newDate = new Date(mostRecentAction.date);
+            newDate.setDate(newDate.getDate() + 1); // Increment by 1 day
+        }
+
         const newAction = {
             focus: focusID,
-            date: new Date(),
+            date: newDate,
             created_by: user?.uid
         };
         const { error } = await supabaseConnection.from('focus_actions').insert(newAction);
         if (error) {
             console.error('Error inserting focus:', error);
         }
-    }
+    };
 
     const updateAction = debounce(async (actionID: string, amount: number) => {
         const { error } = await supabaseConnection.from('focus_actions').update({ amount }).eq('id', actionID);
